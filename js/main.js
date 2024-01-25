1a6c203c1deeb012d867442a74326b82e370e52a
hernanvelazquez <hernan166n@gmail.com>
2024-01-25 10:44:55 -0300

    Primera pre entrega JS

---
+++ js/main.js
@@ -0,0 +1,70 @@
+/*SELECCION DE PAIS INICIO
+
+ let pais = parseInt(prompt("Ingrese país de envío: 1 Argentina , 2 Brasil , 3 Bolivia , 4 Chile , 5 Paraguay , 6 Uruguay"))
+
+ switch(pais) {
+    case 1:
+        console.log("Bienvenido a ...")
+        break
+    case 2:
+        console.log("Bienvenido a ...")
+        break
+    case 3:
+        console.log("Bienvenido a ...")
+        break    
+    case 4:
+        console.log("Bienvenido a ...")
+        break
+    case 5:
+        console.log("Bienvenido a ...")
+        break
+    case 6:
+            console.log("Bienvenido a ...")
+            break
+    default:
+        console.log("País no disponible para comprar en esta tienda.")
+        break
+ } */
+
+
+
+ /*bienvenida()
+function bienvenida () {
+    console.log("Bienvenidos a nuestra tienda online.")
+}
+*/
+
+
+ /*LISTA DE PRODUCTOS A VENDER
+
+ const stock = ["producto1", "producto2", "producto3", "producto4"]
+ const cantidad = stock.length;
+
+ console.log(cantidad)
+
+ /*Agrego productos*/
+
+ /*stock.push("productoNuevo")
+ console.log(stock)*/
+
+
+
+
+
+ /*EJEMPLO DE COMPRA JUNTO CON CALCULADOR DE ENVIOS 
+
+let totalCompra = 100 
+alert("El total de tu compra es: " + totalCompra )
+
+
+ const envios = () => {
+    if (totalCompra >= 300) {
+        alert("Tu envío será gratuito")
+    } else {
+        totalCompra += 299
+        alert("El costo de envío es de $49.99")
+    }
+ }
+
+ envios()*/
+ 
